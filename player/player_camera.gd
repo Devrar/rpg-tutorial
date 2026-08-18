5b12efef70b350d975764bbb96981e47e4cef87a
Devrar <lorenzo.demeio@sns.it>
2026-08-18 15:21:28 +0200

    [ft] camera bounds

---
+++ player/player_camera.gd
@@ -0,0 +1,15 @@
+class_name PlayerCamer
+extends Camera2D
+
+
+func _ready() -> void:
+	GlobalLevelManager.tilemap_bounds_changed.connect(update_limits)
+
+
+func update_limits(bounds: Array[Vector2]) -> void:
+	if bounds.size() < 2:
+		return
+	limit_left = int(bounds[0].x)
+	limit_top = int(bounds[0].y)
+	limit_right = int(bounds[1].x)
+	limit_bottom = int(bounds[1].y)
---
+++ tile_maps/level_tile_map.gd
@@ -0,0 +1,17 @@
+class_name LevelTileMap
+extends TileMap
+
+
+func _ready() -> void:
+	GlobalLevelManager.change_tilemap_bounds(get_tile_map_bounds())
+
+
+func get_tile_map_bounds() -> Array[Vector2]:
+	var bounds: Array[Vector2] = []
+	bounds.append(
+		Vector2(get_used_rect().position * rendering_quadrant_size)
+	)
+	bounds.append(
+		Vector2(get_used_rect().end * rendering_quadrant_size)
+	)
+	return bounds
