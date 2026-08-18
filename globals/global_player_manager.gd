243bdbee0efcc19773fb5f6dec35ffc9867d9d54
Devrar <lorenzo.demeio@sns.it>
2026-08-18 19:15:06 +0200

    [ft] player spawn position

--- globals/global_player_manager.gd
+++ globals/global_player_manager.gd
@@ -1,3 +1,21 @@
 extends Node
 
 var player: Player
+var player_spawned: bool = false
+
+const PLAYER = preload("res://player/player.tscn")
+
+
+func _ready() -> void:
+	add_player_instance()
+	await get_tree().create_timer(0.5).timeout
+	player_spawned = true
+
+
+func add_player_instance() -> void:
+	player = PLAYER.instantiate()
+	add_child(player)
+
+
+func set_player_position(new_position: Vector2) -> void:
+	player.global_position = new_position
