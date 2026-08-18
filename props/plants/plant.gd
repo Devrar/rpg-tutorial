4d04b5bde676f98fdef0c79c13ebb75d29b35154
Devrar <lorenzo.demeio@sns.it>
2026-08-18 18:47:51 +0200

    [fix] plant take damage

--- props/plants/plant.gd
+++ props/plants/plant.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	($Hitbox as Hitbox).damage_taken.connect(take_damage)
 
 
-func take_damage(_damage: int) -> void:
+func take_damage(_hurtbox: Hurtbox) -> void:
 	queue_free()
