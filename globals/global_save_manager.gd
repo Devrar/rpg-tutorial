1a27cc3c87a201a17b858e6679418393f7366a07
Devrar <lorenzo.demeio@sns.it>
2026-08-20 18:23:48 +0200

    [ft] inventory save

--- globals/global_save_manager.gd
+++ globals/global_save_manager.gd
@@ -22,6 +22,7 @@ var current_save: Dictionary = {
 func save_game() -> void:
 	update_player_data()
 	update_scene_path()
+	update_item_data()
 	var file := FileAccess.open(SAVE_PATH + "save.sav", FileAccess.WRITE)
 	var save_json := JSON.stringify(current_save)
 	file.store_line(save_json)
@@ -40,6 +41,7 @@ func load_game() -> void:
 
 	GlobalPlayerManager.set_player_position(Vector2(current_save.player.pos_x, current_save.player.pos_y))
 	GlobalPlayerManager.set_player_health(current_save.player.hp, current_save.player.max_hp)
+	GlobalPlayerManager.INVENTORY_DATA.parse_save_data(current_save.items)
 
 	await GlobalLevelManager.level_loaded
 
@@ -61,3 +63,7 @@ func update_scene_path() -> void:
 		if c is Level:
 			path = c.scene_file_path
 	current_save.scene_path = path
+
+
+func update_item_data() -> void:
+	current_save.items = GlobalPlayerManager.INVENTORY_DATA.get_save_data()
--- gui/pause_menu/inventory/scripts/inventory_data.gd
+++ gui/pause_menu/inventory/scripts/inventory_data.gd
@@ -14,7 +14,7 @@ func add_item(item: ItemData, count: int = 1) -> bool:
 			if slot.item_data == item:
 				slot.quantity += count
 				return true
-	
+
 	for i in slots.size():
 		if slots[i] == null:
 			var new_slot = SlotData.new()
@@ -23,14 +23,15 @@ func add_item(item: ItemData, count: int = 1) -> bool:
 			new_slot.changed.connect(slot_changed)
 			slots[i] = new_slot
 			return true
-	
+
 	print("[!] Inventory full")
 	return false
 
 
 func connect_slots() -> void:
 	for s in slots:
-		s.changed.connect(slot_changed)
+		if s:
+			s.changed.connect(slot_changed)
 
 
 func slot_changed() -> void:
@@ -41,3 +42,36 @@ func slot_changed() -> void:
 				var index = slots.find(s)
 				slots[index] = null
 				changed.emit()
+
+
+func get_save_data() -> Array:
+	var item_save: Array = []
+	for i in slots.size():
+		item_save.append(item_to_save(slots[i]))
+	return item_save
+
+
+func item_to_save(slot: SlotData) -> Dictionary:
+	var result = { item_path = "", quantity = 0 }
+	if slot != null:
+		result.quantity = slot.quantity
+		result.item_path = slot.item_data.resource_path
+	return result
+
+
+func parse_save_data(save_data: Array) -> void:
+	var array_size = slots.size()
+	slots.clear()
+	slots.resize(array_size)
+	for i in save_data.size():
+		slots[i] = item_from_save(save_data[i])
+	connect_slots()
+
+
+func item_from_save(save_object: Dictionary) -> SlotData:
+	if save_object.item_path == "":
+		return null
+	var new_slot = SlotData.new()
+	new_slot.item_data = load(save_object.item_path)
+	new_slot.quantity = save_object.quantity
+	return new_slot
