8b7c457e20b3cfb351c933105d6be1cf2ac1748f
Devrar <lorenzo.demeio@sns.it>
2026-08-21 15:48:52 +0200

    [ft] audio manager

--- levels/scripts/level.gd
+++ levels/scripts/level.gd
@@ -1,12 +1,14 @@
 class_name Level
 extends Node2D
 
+@export var music: AudioStream
+
 
 func _ready() -> void:
 	y_sort_enabled = true
 	GlobalPlayerManager.set_as_parent(self)
 	GlobalLevelManager.level_load_started.connect(_free_level)
-
+	GlobalAudioManager.play_music(music)
 
 func _free_level() -> void:
 	GlobalPlayerManager.unparent_player(self)
